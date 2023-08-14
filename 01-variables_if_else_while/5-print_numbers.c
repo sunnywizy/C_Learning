6591b9739d9b6010d73108af689c2333b83d8f0a
sunnywizy <sundayobasi222@gmail.com>
2023-08-15 00:06:14 +0100

    How to print numbers in base 10

---
+++ 01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*A program that prints all single
+digit numbers of base 10 starting from 0*/
+
+int main(void) {
+    int num;
+
+    for(num = 0; num < 10; num++)
+        printf("%d", num);
+        printf("\n");
+
+    return(0);
+}
